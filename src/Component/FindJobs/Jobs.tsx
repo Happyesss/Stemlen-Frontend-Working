8aee97a29f017c22346fe089006dda902e955818
Happyesss <22csaiml002@jssaten.ac.in>
2025-04-17 00:55:03 +0530

    .

--- src/Component/FindJobs/Jobs.tsx
+++ src/Component/FindJobs/Jobs.tsx
@@ -104,7 +104,7 @@ const Jobs = () => {
           <JobCard key={index} {...job} />
         ))}
       </div>
-      <div className='text-base font-semibold text-center text-blue-400 bs-mx:text-lg'>
+      <div className='text-base font-semibold text-center text-blue-400 mt-10'>
         Note: You have reached the end of the page. Job recommendations are tailored based on your location.
       </div>
     </div>
